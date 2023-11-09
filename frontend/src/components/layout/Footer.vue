20da3521901bc1e720d738f04996e8cb3a64a394
John Uzodinma <john.uzodinma@internetbrands.com>
2023-11-09 16:23:18 +0100

    Refactored code to use Vue and SCSS Components and introduced responsiveness for different screen sizes

---
+++ frontend/src/components/layout/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+  <footer class="mainFooter">
+
+    <div class="mainFooter__inner">
+
+      <div class="mainFooter__column">
+        <h3 class="mainFooter__column-heading">Contact the Publisher</h3>
+        <ul class="mainFooter__column-list">
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">mike@runo.com</a>
+          </li>
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">+944 450 904 505</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="mainFooter__column">
+        <h3 class="mainFooter__column-heading">Explorate</h3>
+        <ul class="mainFooter__column-list">
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">About</a>
+          </li>
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">Partners</a>
+          </li>
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">Job Opportunities</a>
+          </li>
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">Advertise</a>
+          </li>
+          <li class="mainFooter__column-listItem">
+            <a class="mainFooter__column-listItem-link" href="#">Membership</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="mainFooter__column">
+        <h3 class="mainFooter__column-heading">Headquarter</h3>
+        <p class="mainFooter__column-text">191 Middleville Road, NY 1001, Sydney Australia</p>
+      </div>
+
+      <div class="mainFooter__column">
+        <h3 class="mainFooter__column-heading">Headquarter</h3>
+        <p class="mainFooter__column-text">191 Middleville Road, NY 1001, Sydney Australia</p>
+      </div>
+
+    </div>
+
+    <div class="mainFooter__bottom">
+      <div class="mainFooter__bottom-inner">
+        <p class="mainFooter__bottom-text">2021 | RUNO Publisher Studio</p>
+        <p class="mainFooter__bottom-text">Subscribe Now</p>
+      </div>
+    </div>
+  </footer>
+</template>
