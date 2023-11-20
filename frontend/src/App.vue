bffd04d61948bd7ebe1f8f4b4ff8092e0b38dae1
John Uzodinma <john.uzodinma@internetbrands.com>
2023-11-20 02:17:40 +0100

    Restored App.vue to avoid conflicts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,39 +2,6 @@
 import { RouterView } from "vue-router";
 </script>
 
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.getElementById('jsonForm');
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        var title = document.getElementById('title').value;
-        var content = document.getElementById('content').value;
-        var data = {
-            title: title,
-            content: content
-        };
-
-        // sendDataToHackerServer(data)
-        // simply return the response of the upstream server
-
-        fetch('http://localhost:8080/articles', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    });
-});
-</script>
-
 <template>
   <header>
    Header
@@ -43,20 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
   <main>
     <RouterView />
   </main>
-
-  <form id="jsonForm">
-    <div>
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" required>
-    </div>
-    <div>
-        <label for="content">Content:</label>
-        <textarea id="content" name="content" rows="4" cols="50" required></textarea>
-    </div>
-    <div>
-        <button type="submit">Submit</button>
-    </div>
-</form>
 
 </template>
 
